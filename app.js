a27b445c446324b71024e67511daaa2081062f42
simon <sboakye.mba@gmail.com>
2024-06-28 20:12:56 +0200

    addition of ip address

--- app.js
+++ app.js
@@ -6,20 +6,22 @@ server.use(express.static("static")); // serve static content from the "static" 
 server.use(express.json()); // tells server that we are expecting json data to handle
 
 // server endpoint to do some basic calculation
-server.post('/calculate', (req, res) => {
-    console.log(req.body);
+server.post("/calculate", (req, res) => {
+  console.log(req.body);
 
-    const op1 = parseFloat(req.body.operand1);
-    const op2 = parseFloat(req.body.operand2);
+  const op1 = parseFloat(req.body.operand1);
+  const op2 = parseFloat(req.body.operand2);
 
-    // calculate the result
-    const result = op1 + op2;
+  // calculate the result
+  const result = op1 + op2;
 
-    // send result back to client
-    res.send({ result: result });
+  // send result back to client
+  res.send({ result: result });
 });
 
 // start the server
-server.listen(PORT, () => {
-    console.log(`Your server is up and running. Plese go to http://localhost:${PORT}/ to start the app.`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(
+    `Your server is up and running. Plese go to http://localhost:${PORT}/ to start the app.`
+  );
 });
